f656125be01ea755aea87469b5b3e053fd27ae42
filipenkosan9 <filipenkosan9@mail.ru>
2016-06-05 13:19:26 +0300

    Added first

---
+++ diagrame1/Directoty.java
@@ -0,0 +1,10 @@
+package ua.goit.module3.diagrame1;
+
+import java.util.List;
+
+/**
+ * Created by Alex on 04.06.2016.
+ */
+public class Directoty {
+    List <File> file;
+}
---
+++ diagrame1/File.java
@@ -0,0 +1,8 @@
+package ua.goit.module3.diagrame1;
+
+/**
+ * Created by Alex on 04.06.2016.
+ */
+public class File {
+
+}
